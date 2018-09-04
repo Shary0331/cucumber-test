836b705f4cb5ac02d22680637806737ee21a68a0
Pióro <klpo@gft.com>
2018-09-04 13:39:11 +0200

    cucumber

---
+++ cucumber-test/target/cucumber/report.js
@@ -0,0 +1,147 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("src/main/java/featureFiles/products.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Products page",
+  "description": "",
+  "id": "products-page",
+  "keyword": "Feature"
+});
+formatter.scenarioOutline({
+  "line": 3,
+  "name": "validate promo code alert is visiblformatter.before({
+  "duration": 48693007350,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 3,
+  "name": "Submit valid data via contact us form",
+  "description": "",
+  "id": "submit-data-to-webdriveruniversity-using-contact-us-form;submit-valid-data-via-contact-us-form",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 4,
+  "name": "I access webdriveruniversity contact us form",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 5,
+  "name": "I enter a valid firstname",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 6,
+  "name": "I enter a valid last name",
+  "rows": [
+    {
+      "cells": [
+        "woods",
+        "jackson",
+        "jones"
+      ],
+      "line": 7
+    }
+  ],
+  "keyword": "When "
+});
+formatter.step({
+  "line": 8,
+  "name": "i enter a valid email address",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 9,
+  "name": "i enter comments",
+  "rows": [
+    {
+      "cells": [
+        "example comment one",
+        "example comment two"
+      ],
+      "line": 10
+    }
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 11,
+  "name": "i click on the submit button",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 12,
+  "name": "the information should successfully be submitted via the contact us form",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "ContactUsSteps.i_access_webdriveruniversity_contact_us_form()"
+});
+formatter.result({
+  "duration": 4914434848,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ContactUsSteps.i_enter_a_valid_firstname()"
+});
+formatter.result({
+  "duration": 3514345060,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ContactUsSteps.i_enter_a_valid_last_name(DataTable)"
+});
+formatter.result({
+  "duration": 3238198893,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ContactUsSteps.i_enter_a_valid_email_address()"
+});
+formatter.result({
+  "duration": 4664481479,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ContactUsSteps.i_enter_comments(DataTable)"
+});
+formatter.result({
+  "duration": 2795866470,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ContactUsSteps.i_click_on_the_submit_button()"
+});
+formatter.result({
+  "duration": 2810698608,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ContactUsSteps.the_information_should_successfully_be_submitted_via_the_contact_us_form()"
+});
+formatter.result({
+  "duration": 915648771,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 315905451,
+  "status": "passed"
+});
+});roductSteps.user_clicks_on(String)"
+});
+formatter.result({
+  "duration": 1995977251,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ProductSteps.user_should_be_presented_with_a_promo_alert()"
+});
+formatter.result({
+  "duration": 5776393482,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 584484892,
+  "status": "passed"
+});
+});
