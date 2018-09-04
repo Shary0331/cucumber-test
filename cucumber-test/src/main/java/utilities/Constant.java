c716fa9e6ac3294990cfb106defe3433135b0af4
kpioro <limak91@gmail.com>
2018-09-04 14:09:48 +0200

    Update Constant.java

--- cucumber-test/src/main/java/utilities/Constant.java
+++ cucumber-test/src/main/java/utilities/Constant.java
@@ -5,7 +5,7 @@ import java.io.File;
 class Constant {
 
     /**Config Properties file **/
-    final static String CONFIG_PROPERTIES_DIRECTORY = System.getProperty("user.dir") + "src" + File.separator + "main"  + File.separator + "resources"  + File.separator + "config.properties";
+    final static String CONFIG_PROPERTIES_DIRECTORY = System.getProperty("user.dir") + "src" + File.pathSeparator + "main"  + File.pathSeparator + "resources"  + File.pathSeparator + "config.properties";
 
     final static String GECKO_DRIVER_DIRECTORY = System.getProperty("user.dir") + "src" + File.separator + "main" + File.separator + "resources" + File.separator + "geckodriver.exe";
 
