35448533fec128106e920619cbaf63f15ae11318
Pióro <klpo@gft.com>
2018-09-04 14:01:34 +0200

    file separator

--- cucumber-test/src/main/java/utilities/Constant.java
+++ cucumber-test/src/main/java/utilities/Constant.java
@@ -1,13 +1,15 @@
 package utilities;
 
+import java.io.File;
+
 class Constant {
 
     /**Config Properties file **/
-    final static String CONFIG_PROPERTIES_DIRECTORY = System.getProperty("user.dir") + "\\src\\main\\resources\\config.properties";
+    final static String CONFIG_PROPERTIES_DIRECTORY = System.getProperty("user.dir") + "src" + File.separator + "main"  + File.separator + "resources"  + File.separator + "config.properties";
 
-    final static String GECKO_DRIVER_DIRECTORY = System.getProperty("user.dir") + "\\src\\main\\resources\\geckodriver.exe";
+    final static String GECKO_DRIVER_DIRECTORY = System.getProperty("user.dir") + "src" + File.separator + "main" + File.separator + "resources" + File.separator + "geckodriver.exe";
 
-    final static String CHROME_DRIVER_DIRECTORY = System.getProperty("user.dir") + "\\src\\main\\resources\\chromedriver.exe";
+    final static String CHROME_DRIVER_DIRECTORY = System.getProperty("user.dir") + "src" + File.separator + "main" + File.separator + "resources" + File.separator + "chromedriver.exe";
 
-    final static String IE_DRIVER_DIRECTORY = System.getProperty("user.dir") + "src\\main\\resources\\IEDriverServer.exe";
+    final static String IE_DRIVER_DIRECTORY = System.getProperty("user.dir") + "src" + File.separator + "main" + File.separator + "resources" + File.separator + "IEDriverServer.exe";
 }
