55212961fea77ea3d7034e6f6d154547c29c8a26
kpioro <limak91@gmail.com>
2018-09-05 13:57:22 +0200

    Update DriverFactory.java

--- cucumber-test/src/main/java/utilities/DriverFactory.java
+++ cucumber-test/src/main/java/utilities/DriverFactory.java
@@ -14,6 +14,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import pageObjects.*;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
