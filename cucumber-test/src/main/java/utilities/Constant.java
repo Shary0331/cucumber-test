9f21b7ca7e777fefb7075f114ac2bb410547ef61
kpioro <limak91@gmail.com>
2018-09-05 10:36:49 +0200

    Update Constant.java

--- cucumber-test/src/main/java/utilities/Constant.java
+++ cucumber-test/src/main/java/utilities/Constant.java
@@ -9,7 +9,9 @@ class Constant {
     /**Config Properties file **/
   //  final static String CONFIG_PROPERTIES_DIRECTORY = Constant.class.getResource("/config.properties").getPath();
     
-    final static String CONFIG_PROPERTIES_DIRECTORY = Paths.get(System.getProperty("user.dir"), "src", "main", "resources", "config.properties").toString();
+//    final static String CONFIG_PROPERTIES_DIRECTORY = Paths.get(System.getProperty("user.dir"), "src", "main", "resources", "config.properties").toString();
+  
+    final static String CONFIG_PROPERTIES_DIRECTORY = System.getProperty("user.dir") + "/src/main/resources/config.properties";
     
 //   final static String CONFIG_PROPERTIES_DIRECTORY = System.getProperty("user.dir") + File.separator + "src" + File.separator + "main"  + File.separator + "resources"  + File.separator + "config.properties";
 
