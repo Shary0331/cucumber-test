36e6342c70cdce7258d993e92507870b8e257d37
kpioro <limak91@gmail.com>
2018-09-05 15:52:09 +0200

    Update DriverFactory.java

--- cucumber-test/src/main/java/utilities/DriverFactory.java
+++ cucumber-test/src/main/java/utilities/DriverFactory.java
@@ -43,6 +43,7 @@ public class DriverFactory {
             String browserName = properties.getProperty("browser");
             String profile = System.getProperty("profileId");
 //            logger.info("Current profile: " + profile);
+            logger.info("Browser type: " + browserName);
 
 //            ReadConfigFile file = new ReadConfigFile();
 //            String browserName = file.getBrowser();
